c2eecc24744ad84b493d190f6bb397110e3f3474
Magno Belloni <39836481+MagnoBelloni@users.noreply.github.com>
2024-11-09 16:15:00 +0000

    Initial Structure

---
+++ pages/index.js
@@ -0,0 +1,9 @@
+ function Home() {
+    return <div>
+                <h1>Te Adoro, você é uma pessoa incrivel</h1>
+                <p>estou muito orgulhoso de você, você demonstrou ser uma pessoa forte nesse tempo que passou, nesse novo ciclo você vai atingir todos os seus objetivos❤</p>
+                <img src="https://img.freepik.com/fotos-premium/adoraveis-personagens-de-gatos-3d-usam-roupas-coloridas-fofas-e-engracadas_784625-1107.jpg"></img>
+            </div>
+ }
+
+ export default Home;
