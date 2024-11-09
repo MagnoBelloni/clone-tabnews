93a44da8318c77f045c175188ad2ee772a444eb4
Magno Belloni <39836481+MagnoBelloni@users.noreply.github.com>
2024-11-09 16:21:51 +0000

    Vercel deployment test

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
  function Home() {
     return <div>
                 <h1>Te Adoro, você é uma pessoa incrivel</h1>
-                <p>estou muito orgulhoso de você, você demonstrou ser uma pessoa forte nesse tempo que passou, nesse novo ciclo você vai atingir todos os seus objetivos❤</p>
+                <p>estou muito orgulhoso de você, você demonstrou ser uma pessoa forte nesse tempo que passou, nesse novo ciclo você vai atingir todos os seus objetivos❤😉</p>
                 <img src="https://img.freepik.com/fotos-premium/adoraveis-personagens-de-gatos-3d-usam-roupas-coloridas-fofas-e-engracadas_784625-1107.jpg"></img>
             </div>
  }
