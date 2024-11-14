ab7aeea5dd1151176c32f847106b713d3cfeabfc
Magno Belloni <39836481+MagnoBelloni@users.noreply.github.com>
2024-11-14 01:10:07 +0000

    Added prettier configuration

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # clone-tabnews
+
 Implementação do clone do https://www.tabnews.com.br feito durante o curso.dev
--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,15 @@
- function Home() {
-    return <div>
-                <h1>Te Adoro, você é uma pessoa incrivel</h1>
-                <p>estou muito orgulhoso de você, você demonstrou ser uma pessoa forte nesse tempo que passou, nesse novo ciclo você vai atingir todos os seus objetivos❤😉</p>
-                <img src="https://img.freepik.com/fotos-premium/adoraveis-personagens-de-gatos-3d-usam-roupas-coloridas-fofas-e-engracadas_784625-1107.jpg"></img>
-            </div>
- }
+function Home() {
+    return (
+        <div>
+            <h1>Te Adoro, você é uma pessoa incrivel</h1>
+            <p>
+                estou muito orgulhoso de você, você demonstrou ser uma pessoa
+                forte nesse tempo que passou, nesse novo ciclo você vai atingir
+                todos os seus objetivos❤😉
+            </p>
+            <img src="https://img.freepik.com/fotos-premium/adoraveis-personagens-de-gatos-3d-usam-roupas-coloridas-fofas-e-engracadas_784625-1107.jpg"></img>
+        </div>
+    );
+}
 
- export default Home;
+export default Home;
