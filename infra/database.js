9e12530fcf8efb34192540c754900d035d0857a5
Magno Belloni <39836481+MagnoBelloni@users.noreply.github.com>
2024-12-02 20:58:40 +0000

    Implemented connection with DBs outside local enviroment

--- infra/database.js
+++ infra/database.js
@@ -6,7 +6,8 @@ async function query(queryObject) {
         port: process.env.POSTGRES_PORT,
         user: process.env.POSTGRES_USER,
         database: process.env.POSTGRES_DB,
-        password: process.env.POSTGRES_PASSWORD
+        password: process.env.POSTGRES_PASSWORD,
+        ssl: process.env.NODE_ENV == 'development' ? false : true
     })
 
     try {
@@ -17,6 +18,7 @@ async function query(queryObject) {
         return result
     } catch (error) {
         console.log(error)
+        throw error
     } finally {
         await client.end()
     }
